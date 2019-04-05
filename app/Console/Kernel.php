f94c77ee0f74ad9786686771907ee925de8ea214
BillTran <trananh211@gmail.com>
2019-04-06 04:09:16 +0700

    composer autoload errror

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         $schedule->call('App\Http\Controllers\GoogleController@fulfillment')->dailyAt('00:30');
         /*Upload file image lên thư mục fulfill*/
         $schedule->call('App\Http\Controllers\GoogleController@uploadFileDriver')
-            ->everyTenMinutes()->between('1:00', '5:00')->withoutOverlapping(10);
+            ->everyTenMinutes()->between('1:00', '5:00');
         $schedule->call('App\Http\Controllers\ApiController@checkPaymentAgain')->dailyAt('23:00')
         ->skip( function(){
             return false;
