d7db767e8185d7180a96351556ad2c28a36ed821
BillTran <trananh211@gmail.com>
2019-04-08 15:48:39 +0700

    test fulfill

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,15 +26,17 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call('App\Http\Controllers\GoogleController@fulfillment')->everyMinute();
+        $schedule->call('App\Http\Controllers\GoogleController@uploadFileDriver')->everyMinute();
+        $schedule->call('App\Http\Controllers\ApiController@checkPaymentAgain')->everyMinute();
         /*Export file excel lên thư mục fulfill*/
-        $schedule->call('App\Http\Controllers\GoogleController@fulfillment')->dailyAt('00:30');
+        //$schedule->call('App\Http\Controllers\GoogleController@fulfillment')->dailyAt('00:30');
         /*Upload file image lên thư mục fulfill*/
-        $schedule->call('App\Http\Controllers\GoogleController@uploadFileDriver')
-            ->everyTenMinutes()->between('1:00', '5:00');
-        $schedule->call('App\Http\Controllers\ApiController@checkPaymentAgain')->dailyAt('23:00')
+        //$schedule->call('App\Http\Controllers\GoogleController@uploadFileDriver')->everyTenMinutes()->between('1:00', '5:00');
+        /*$schedule->call('App\Http\Controllers\ApiController@checkPaymentAgain')->dailyAt('23:00')
         ->skip( function(){
             return false;
-        });
+        });*/
     }
 
     /**
