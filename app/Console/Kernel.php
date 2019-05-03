9969294278078ea851b5705f313f8c13b905cce3
BillTran <trananh211@gmail.com>
2019-05-04 01:00:20 +0700

    step 2: done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,11 +28,11 @@ class Kernel extends ConsoleKernel
         $schedule->exec('chmod -R 777 '.storage_path())->dailyAt('00:20');
 
         /** Run every minute specified queue if not already started */
-        if (stripos((string) shell_exec('ps xf | grep \'[q]ueue:work\''), 'artisan queue:work') === false) {
+        /*if (stripos((string) shell_exec('ps xf | grep \'[q]ueue:work\''), 'artisan queue:work') === false) {
             $schedule->command('queue:work --queue=default --sleep=2 --tries=3 --timeout=5')
                 ->everyMinute()->appendOutputTo(storage_path() . '/logs/scheduler.log');
-        }
-
+        }*/
+        $schedule->command('queue:work --stop-when-empty')->everyFiveMinutes();
         /*Export file excel lên thư mục fulfill*/
         $schedule->call('App\Http\Controllers\GoogleController@fulfillment')->dailyAt('00:30');
         /*Upload file image lên thư mục fulfill*/
