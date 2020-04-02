f50935eff94ad640074b061eaff28080d0f897e7
BillTran <trananh211@gmail.com>
2020-04-02 17:32:30 +0700

    where molofa

--- app/Api.php
+++ app/Api.php
@@ -1991,6 +1991,7 @@ class Api extends Model
     {
         $products = \DB::table('woo_products')
             ->select('id','woo_info_id','product_id', 'image')
+            ->where('woo_info_id', 4)
             ->get()->toArray();
         $data_update = array();
         foreach ($products as $item)
