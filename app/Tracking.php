6f377f5bdd4c08bd5b614c827147d91373b47e0e
BillTran <trananh211@gmail.com>
2020-05-09 16:15:18 +0700

    done

--- app/Tracking.php
+++ app/Tracking.php
@@ -149,12 +149,14 @@ class Tracking extends Model
             {
                 if ($status == '1')
                 {
+                    $order_status = order_status();
                     $list_trackings_available = \DB::table('trackings')
                         ->where('status','>',env('TRACK_NOTFOUND'))->pluck('order_id')->toArray();
                     $lists = \DB::table('woo_orders as wod')
                         ->select('wod.number', 'wod.created_at', 'wod.updated_at', 'wod.status')
                         ->whereNotIn('number', $list_trackings_available)
                         ->whereBetween('wod.status', [env('STATUS_WORKING_DONE'), env('STATUS_WORKING_MOVE')])
+                        ->whereIn('order_status', $order_status)
                         ->orderBy('wod.number','ASC')
                         ->paginate($paginate)->toArray();
                     $lists = json_decode(json_encode($lists, true), true);
